ea17d00cc4ddf19507ecbbf55920997e0324371c
Ahmad Oriza <ahmadoriza@gmail.com>
2023-04-05 19:42:04 +0700

    Change video to youtube

--- resources/views/knowledge/video.blade.php
+++ resources/views/knowledge/video.blade.php
@@ -16,20 +16,21 @@
         <div class="row">
 
         @foreach ($posts as $post)
-            <div class="col-md-4">
-                <div class="card mb-4">
-                    <video style="width:100%;" height="240" controls>
-                        <source src="{{ url('videos/' . $post->attachment) }}" type="video/mp4">
-                        Your browser does not support the video tag.
-                    </video> 
+            <div class="col-md-6">
 
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $post->title }}</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <small>Dibuat pada {{ $post->created_at }}</small>
+                <div class="mb-4">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/{{ $post->attachment }}?rel=0" allowfullscreen></iframe>
                     </div>
+
+                    <h5 class="card-title">{{ $post->title }}</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <small>100 kali ditonton</small> . <small>Diunggah pada {{ $post->created_at }}</small>
+
                 </div>
+
             </div>
+
         @endforeach
 
         </div>
