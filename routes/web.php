98f2e180fec190534e71fa5742ef78e798ba57c8
AldianBaim <aldiansyahibrahim5@gmail.com>
2023-04-10 06:22:24 +0700

    Update redirect when logout to login page

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('login');
 });
 
 Route::get('/users', [UserController::class, 'index'])->middleware('auth:sanctum');;
