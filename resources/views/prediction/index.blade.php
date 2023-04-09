b42f15dfb1d090846ab6063f2acdd4b03e5d79f4
Ahmad Oriza <ahmadoriza@gmail.com>
2023-04-10 00:11:44 +0700

    Setup prediction page

--- resources/views/prediction/index.blade.php
+++ resources/views/prediction/index.blade.php
@@ -12,10 +12,30 @@
     <h4>Prediction</h4>
     <p><small>Prediksi informasi penting seputar Aglaonema, dalam beberapa tahun kedepan.</small></p>
 
-    <div>
+    <div class="row">
+        <div class="col">
 
-        Test.
-    
+        </div>
+        <div class="col">
+            <div class="pull-right"><a href="" onclick="location.reload()"><span class="fa fa-refresh"></span> Refresh data</a></div>
+        </div>
+    </div>
+
+    <div class="mb-3 mt-3">
+
+    <div class="row">
+
+        <div class="col-md-6">
+            <img class="w-100" src="https://i.postimg.cc/ZK4d6Kgw/Whats-App-Image-2023-04-09-at-07-24-41.jpg" />
+            <img class="w-100" src="https://i.postimg.cc/B65vv2q4/Whats-App-Image-2023-04-09-at-20-55-09-1.jpg" />
+            <img class="w-100" src="https://i.postimg.cc/DZ4qpjd5/Whats-App-Image-2023-04-09-at-07-23-48.jpg" />
+        </div>
+
+        <div class="col-md-6">
+            <img class="w-100" src="https://i.postimg.cc/c1KDPT4p/Whats-App-Image-2023-04-09-at-07-23-26.jpg" />
+            <img class="w-100" src="https://i.postimg.cc/T3kgdMtC/Whats-App-Image-2023-04-09-at-07-24-22.jpg" />
+        </div>
+
     </div>
 </section>
 
